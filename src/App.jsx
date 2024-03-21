9f9079c24fee5f4849278b566b5aa5e55c025623
Poliana Schuelter <polianaschuelter@polianas-mbp.speedport.ip>
2024-03-21 14:42:28 +0100

    final group work

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import RandomPicker from './components/RandomPicker'
+
+function App() {
+  return (
+    <div style={{ margin: 0 }}>
+      <RandomPicker />
+    </div>
+  )
+}
+
+export default App
